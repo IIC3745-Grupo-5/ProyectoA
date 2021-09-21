66964e03cdd5e5c678336e23d8b54b19a7d083ed
Diego Solari <dasolari@uc.cl>
2021-09-21 12:32:36 -0300

    feat: constants for global use

---
+++ lib/constants/dimensions.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Dimensions
+  BOARD = {
+    'BEGGINER' => [7, 7], # 8x8
+    'INTERMEDIATE' => [11, 11], # 12x12
+    'EXPERT' => [15, 15] # 16x16
+  }
+
+  MINES = {
+    'BEGGINER' => 30, # 46%
+    'INTERMEDIATE' => 66, # 46%
+    'EXPERT' => 118 # 46%
+  }
+end
