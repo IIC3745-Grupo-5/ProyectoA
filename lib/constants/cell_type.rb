947fa7785badced5b4c8a9f6c4970d96607ded50
Diego Solari <dasolari@uc.cl>
2021-09-22 12:17:05 -0300

    feat: added preliminary marking of cells

--- lib/constants/cell_type.rb
+++ lib/constants/cell_type.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 module CellType
-  HIDDEN = '🔳'
-  SAFE = '⬜️'
+  HIDDEN = '⬜️'
+  SAFE = '🔳'
   MINE = '💣'
   FLAGGED = '🚩'
 end
--- lib/main.rb
+++ lib/main.rb
@@ -2,6 +2,26 @@
 
 require_relative './board'
 require_relative './constants/difficulty_type'
+require 'highline/import'
 
-board = Board.new(DifficultyType::INTERMEDIATE)
+# Toda esta lógica se tiene que mover a la clase Game
+board = Board.new(DifficultyType::BEGGINER)
 board.print
+
+playing = true
+loop do
+  choose do |menu|
+    menu.prompt = 'What do you want to do?'
+    menu.choice(:'Discover a cell') {
+      y_coordinate = ask 'In which row?: '
+      x_coordinate = ask 'In which column?: '
+      board.discover_cell(x_coordinate.to_i, y_coordinate.to_i)
+    }
+    menu.choice(:'Flag or unflag a cell') { say('Not implemented yet...') }
+    menu.choice(:Quit) { playing = false }
+  end
+  playing && board.print
+  break unless playing
+end
+
+puts 'Good Bye!'
